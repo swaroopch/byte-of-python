caf6e74ca8b87e9b3d5eef11c41f3edd03b54b5e
Swaroop C H <swaroop@swaroopch.com>
2018-08-22 19:22:19 -0700

    Remove unnecessary call to `print`
    Thanks 杜小航!

--- programs/function_varargs.py
+++ programs/function_varargs.py
@@ -9,4 +9,4 @@ def total(a=5, *numbers, **phonebook):
     for first_part, second_part in phonebook.items():
         print(first_part,second_part)
 
-print(total(10,1,2,3,Jack=1123,John=2231,Inge=1560))
+total(10,1,2,3,Jack=1123,John=2231,Inge=1560)
